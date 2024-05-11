243565feb678c559c1b9b25e6fe06b2c9ca6bc19
dcmomo <mohammadjavad.moshiri@tuni.fi>
2024-05-10 22:56:05 -0700

    Add Jinja Template

---
+++ README_Temp.md
@@ -0,0 +1,22 @@
+# About Me
+
+## LeetCode
+- Top Percentage Rating: 3.44%
+- ![Streak](https://image.flaticon.com/icons/png/512/123/123123.png) Streak: 300 Days
+![Knight Top 25% site-wide
+(17856 total)](https://leetcode.com/static/images/badges/knight.png)
+
+![C++](https://img.shields.io/badge/C++-%20248-333333?colorA=4CAF50)
+![Java](https://img.shields.io/badge/Java-%20292-333333?colorA=4CAF50)
+![Go](https://img.shields.io/badge/Go-%20179-333333?colorA=4CAF50)
+![Python3](https://img.shields.io/badge/Python3-%20180-333333?colorA=4CAF50)
+## Difficulty Stats
+
+- EASY Difficulty: 98.95% of Peers Beaten
+
+- MEDIUM Difficulty: 98.59% of Peers Beaten
+
+- HARD Difficulty: 99.07% of Peers Beaten
+
+
+## Projects
--- update_readme.py
+++ update_readme.py
@@ -1,6 +1,7 @@
 import json
 from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
+from jinja2 import Environment, FileSystemLoader
 
 with open("leetcode_query.gql", "r") as gql_file:
     graphql_query = gql(gql_file.read())
@@ -8,12 +9,17 @@ with open("leetcode_query.gql", "r") as gql_file:
 with open("leetcode_query_variables.json", "r") as params_file:
     query_variables = json.load(params_file)
 
-http_transport = RequestsHTTPTransport(
-    url="https://leetcode.com/graphql",
-)
-
+http_transport = RequestsHTTPTransport(url="https://leetcode.com/graphql")
 graphql_client = Client(transport=http_transport)
 
 query_result = graphql_client.execute(graphql_query, variable_values=query_variables)
 
-print(query_result)
+file_loader = FileSystemLoader(".")
+env = Environment(loader=file_loader)
+
+template = env.get_template("README.Jinja")
+
+rendered_template = template.render(data=query_result)
+
+with open("README_Temp.md", "w") as readme_file:
+    readme_file.write(rendered_template)
